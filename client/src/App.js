79ddc4cf344b0d3602203ab5129fd1312818894a
Alex J. Lee <alexjlee.dev@gmail.com>
2021-12-16 15:22:35 +0900

    Fix : Move BrowserRouter from App.js to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Home from './pages/Home';
 import Search from './pages/Search';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
