454e9a3acb79b4e2529149ff20807330d6ffa3d8
Wi <widalida26@gmail.com>
2021-12-17 12:38:25 +0900

    Update : Get episode information

--- server/controllers/search/drama.js
+++ server/controllers/search/drama.js
@@ -1,58 +1,84 @@
 //const { getDrama } = require('../../../client/src/api/DramaDataAPI');
+const { epiosode_info } = require('../../models/episode_info');
 const axios = require('axios');
 
 module.exports = (req, res) => {
   let dramaId = req.query['drama-id'];
+  let seasonIndex = 1;
   console.log(process.env.TMDB_API_KEY);
   console.log(dramaId);
-  // const episodeInfo = {
-  //   'id': 'id',
-  //   'dramaId'
+
+  // const dramaInfo = {
+  //   title: '',
+  //   year: '',
+  //   genres: [],
+  //   overview: '',
+  //   seasonNum: '',
+  //   episodeNum: '',
   // };
-  axios
-    .get(
-      `https://api.themoviedb.org/3/tv/1339?api_key=12020cc123e40be63cf1fc64c2795f38&language=en-US`,
-      //`https://api.themoviedb.org/3/search/tv/?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${keyword}&include_adult=false`,
-      {
-        withCredentials: false,
-      }
-    )
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  //     .then((result) => {
-  //       // console.log(result);
-  //       return result.data.results;
-  //     });
-  // const sendAPICall = aysnc () => {
-  // const info = await getDrama();
-  // }
+  const episodeInfos = [];
+  const episodeInfo = {
+    id: '',
+    dramaId: '',
+    dranaName: '',
+    seasonIndex: '',
+    episodeIndex: '',
+    commentNum: '',
+  };
+  console.log(epiosode_info);
   res.end();
-  //   console.log(444, Users);
-  //   console.log(666, req.body);
-  //   const { user_id, password } = req.body;
-  //   console.log(555, user_id, password);
-  //   Users.findOne({
-  //     where: {
-  //       user_id,
-  //       password,
-  //     },
+  // epiosode_info
+  //   .findOne({
+  //     where: {},
   //   })
-  //     .then((data) => {
-  //       console.log(999, data);
-  //       if (!data) {
-  //         return res.status(404).send('invalid user');
-  //       } else {
-  //         delete data.dataValues.password;
-  //         const accessToken = generateAccessToken(data.dataValues);
-  //         sendAccessToken(res, accessToken);
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       return res.status(500).send('err');
-  //       console.log(err);
-  //     });
+  //   .then((data) => {
+  //     return res.end();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     return res.status(500).send('err');
+  //   });
+  // db에 해당 에피소드 정보가 없을 때
+  // tmdb에서 해당 드라마 정보 받기
+
+  // axios
+  //   .get(
+  //     `https://api.themoviedb.org/3/tv/${dramaId}/season/${seasonIndex}?api_key=${process.env.TMDB_API_KEY}&language=en-US`,
+  //     //`https://api.themoviedb.org/3/tv/${dramaId}?api_key=${process.env.TMDB_API_KEY}&language=en-US`,
+  //     //`https://api.themoviedb.org/3/search/tv/?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${keyword}&include_adult=false`,
+  //     {
+  //       withCredentials: false,
+  //     }
+  //   )
+  //   .then((result) => {
+  //     let data = result.data.episodes;
+  //     console.log(data);
+  //     for (let i = 0; i < data.length; i++) {
+  //       let info = {};
+  //       info.id = data[i].id;
+  //       info.dramaId = dramaId;
+  //       info.seasonIndex = seasonIndex;
+  //       info.episodeIndex = i + 1;
+  //       info.commentNum = 0;
+  //       episodeInfos.push(info);
+  //     }
+  //     res.status(200).json(episodeInfos);
+
+  //     // let data = result.data;
+  //     // dramaInfo.title = data.name;
+  //     // dramaInfo.year = data.first_air_date.slice(0, 4);
+  //     // let genres = data.genres;
+  //     // genres.forEach((genre) => {
+  //     //   dramaInfo.genres.add(genre.name);
+  //     // });
+  //     // dramaInfo.overview = data.overview;
+  //     // //dramaInfo.seasonNum = data.seasonNum;
+  //     // let seasons = data.seasons;
+  //     // for (let i = 0; i < seasons.length; i++) {}
+  //     // //dramaInfo.episodeNums =
+  //     // console.log(dramaInfo);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).send('err');
+  //   });
 };
