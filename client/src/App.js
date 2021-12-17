45e7a62dbdc2c297907a0d10f5e95e4017bbe8a6
Alex J. Lee <alexjlee.dev@gmail.com>
2021-12-18 01:48:56 +0900

    Update : Buttons, Navbar styles

--- client/src/App.js
+++ client/src/App.js
@@ -43,6 +43,7 @@ function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
         {/* <Route
           exact
--- client/src/components/TextButton.js
+++ client/src/components/TextButton.js
@@ -1,34 +1,67 @@
 import styled from 'styled-components';
+import { colors } from '../styles/Colors';
 
-const StyledButton = styled.button`
+const SolidButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+
+  /* 크기 */
+  font-size: 1rem;
+  width: ${(props) => (props.width === 'fit' ? 'fit-content' : '100%')};
+
+  /* 색상 */
+  background-color: ${(props) => colors[props.color]};
+  border: 2px solid ${(props) => colors[props.color]};
+  color: ${colors.white};
+
+  &:hover {
+    opacity: 0.75;
+  }
+`;
+
+const TransparentButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
   align-items: center;
   outline: none;
   border: none;
-  border-radius: 4px;
-  color: black;
-  font-weight: bold;
+  background: none;
   cursor: pointer;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  margin-left: 1rem;
+  padding: 0.5rem 1rem;
 
   /* 크기 */
-  height: 2.25rem;
   font-size: 1rem;
 
   /* 색상 */
-  background: #eee;
+  color: ${(props) => colors[props.color]};
+  border: 2px solid ${(props) => colors[props.color]};
 
-  /* 기타 */
-  &:not(:first-child) {
-    margin-left: 1rem;
+  &:hover {
+    opacity: 0.75;
   }
 `;
 
-function TextButton({ children }) {
-  return <StyledButton>{children}</StyledButton>;
+function TextButton({ children, color, isTransparent, width, onClick }) {
+  return (
+    <>
+      {isTransparent ? (
+        <TransparentButton color={color} width={width} onClick={onClick}>
+          {children}
+        </TransparentButton>
+      ) : (
+        <SolidButton color={color} width={width} onClick={onClick}>
+          {children}
+        </SolidButton>
+      )}
+    </>
+  );
 }
 
 export default TextButton;
