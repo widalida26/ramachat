cf8a92f9db85dcc478f1e51ff6054f02a3828cf8
Wi <widalida26@gmail.com>
2021-12-18 00:50:19 +0900

    Add : Update comment number

--- server/controllers/comments/add.js
+++ server/controllers/comments/add.js
@@ -17,11 +17,11 @@ module.exports = (req, res) => {
       episodeIndex,
       episodeId,
       parentCommentId,
-      isEmpty,
+      commentNum,
     } = req.body;
 
     // Episode_infos 테이블에 해당 에피소드 아이디를 가진 값이 없을 때  => 첫 댓글
-    if (isEmpty) {
+    if (commentNum === 0) {
       // 에피소드 정보 객체 세팅
       let epiInfo = {
         id: episodeId,
@@ -29,13 +29,14 @@ module.exports = (req, res) => {
         drama_name: dramaName,
         season_index: seasonIndex,
         episode_index: episodeIndex,
-        comment_num: 0,
       };
       // Episode_infos 테이블에 에피소드 정보 추가
       Episode_infos.create(epiInfo)
         .then((result) => {})
         // 에피소드 정보 삽입 실패
         .catch((err) => {
+          console.log('episode information insertion failed');
+          console.log(err);
           //res.status(500).send('episode information insertion failed');
         });
 
@@ -48,15 +49,39 @@ module.exports = (req, res) => {
       };
       // 댓글을 Comments 테이블에 삽입
       Comments.create(newComment)
-        .then((result) => {})
+        .then((result) => {
+          console.log(result);
+        })
         // 댓글 정보 삽입 실패
         .catch((err) => {
-          res.status(500).send('err');
+          console.log('comment information insertion failed');
+          console.log(err);
+        });
+
+      // 댓글 숫자 업데이트
+      let newCommentNum = commentNum + 1;
+      Episode_infos.update(
+        {
+          comment_num: newCommentNum,
+        },
+        {
+          where: {
+            id: episodeId,
+          },
+        }
+      )
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log('comment number update failed');
+          console.log(err);
+          //res.status(500).send('err');
         });
       // parentEpisodeId가 있을 때 => 답글 O
-      if (parentCommentId) {
-        console.log();
-      }
+      // if (parentCommentId) {
+      //   console.log();
+      // }
       // 인증 실패
     } else {
       res.status(401).send('unauthorized user');
@@ -64,56 +89,3 @@ module.exports = (req, res) => {
     res.end();
   }
 };
-
-//   let drama_id = req.query['drama-id'];
-//   let season_index = req.query['season-index'];
-//   const episodeInfos = [];
-//   Episode_info.findAll({
-//     where: {
-//       drama_id,
-//       season_index,
-//     },
-//   })
-//     .then((data) => {
-//       // db에 해당 에피소드 정보가 있을 때
-//       // sql db에서 에피소드 정보 받기
-//       if (data.length > 0) {
-//         for (let i = 0; i < data.length; i++) {
-//           //console.log(data[i].dataValues);
-//           let val = data[i].dataValues;
-//           let info = {};
-//           info.id = val.id;
-//           info.episodeIndex = val.episode_index;
-//           info.commentNum = val.comment_num;
-//           episodeInfos.push(info);
-//         }
-//         res.status(200).json(episodeInfos);
-//         // db에 해당 에피소드 정보가 없을 때
-//         // tmdb에서 에피소드 정보 받기
-//       } else {
-//         axios
-//           .get(
-//             `https://api.themoviedb.org/3/tv/${drama_id}/season/${season_index}?api_key=${process.env.TMDB_API_KEY}&language=en-US`,
-//             {
-//               withCredentials: false,
-//             }
-//           )
-//           .then((result) => {
-//             let data = result.data.episodes;
-//             for (let i = 0; i < data.length; i++) {
-//               let info = {};
-//               info.id = data[i].id;
-//               info.episodeIndex = i + 1;
-//               info.commentNum = 0;
-//               episodeInfos.push(info);
-//             }
-//             res.status(200).json(episodeInfos);
-//           })
-//           .catch((err) => {
-//             res.status(500).send('err');
-//           });
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).send('err');
-//     });
