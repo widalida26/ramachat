d3ddbdf637e05eaddc0b8a4f6387db5c5fe06238
Wi <widalida26@gmail.com>
2021-12-17 11:17:27 +0900

    Add : Get information from tmdb

--- server/controllers/search/drama.js
+++ server/controllers/search/drama.js
@@ -1,7 +1,35 @@
+//const { getDrama } = require('../../../client/src/api/DramaDataAPI');
+const axios = require('axios');
+
 module.exports = (req, res) => {
   let dramaId = req.query['drama-id'];
+  console.log(process.env.TMDB_API_KEY);
   console.log(dramaId);
-
+  // const episodeInfo = {
+  //   'id': 'id',
+  //   'dramaId'
+  // };
+  axios
+    .get(
+      `https://api.themoviedb.org/3/tv/1339?api_key=12020cc123e40be63cf1fc64c2795f38&language=en-US`,
+      //`https://api.themoviedb.org/3/search/tv/?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${keyword}&include_adult=false`,
+      {
+        withCredentials: false,
+      }
+    )
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //     .then((result) => {
+  //       // console.log(result);
+  //       return result.data.results;
+  //     });
+  // const sendAPICall = aysnc () => {
+  // const info = await getDrama();
+  // }
   res.end();
   //   console.log(444, Users);
   //   console.log(666, req.body);
