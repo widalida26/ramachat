4e61f59c444aa646d1680fef5b240ff05d313df0
Wi <widalida26@gmail.com>
2021-12-20 09:46:22 +0900

    Fix : Set cors credentials

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     orogin: [`*`],
     //origin: [`http://localhost:3000`],
-    credentials: true,
+    //credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 );
