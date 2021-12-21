23d718d1cc6d483d8c7e0c464da56bd47d8ed6fc
Wi <widalida26@gmail.com>
2021-12-21 14:34:01 +0900

    Add : Like or cancel

--- server/controllers/comments/like.js
+++ server/controllers/comments/like.js
@@ -3,8 +3,7 @@ const { isAuthorized } = require('../tokenFunctions');
 
 module.exports = async (req, res) => {
   let liked = false; // Likes 테이블에 정보가 삽입되었는지 여부
-  let userId = -1; // 좋아요한 사용자 아이디
-  let targetId = -1; // 좋아요된 댓글 아이디
+  let newLike = {}; // 좋아요 객체
 
   try {
     const accessTokenData = isAuthorized(req.cookies);
@@ -12,22 +11,33 @@ module.exports = async (req, res) => {
     if (accessTokenData === null) {
       res.status(401).send('unauthorized user');
       return;
-      // 인증 성공
     }
+
+    // 인증 성공
+    // 좋아요 객체 세팅
     userId = accessTokenData.id;
-    targetId = req.body.targetId;
+    targetId = req.params.commentId;
+    newLike = { userId, targetId };
+
+    // 좋아요 정보 삽입
     liked = await Likes.findOrCreate({
-      where: { userId, targetId },
-      defaults: {
-        userId,
-        findId,
-      },
-    })[1];
+      where: newLike,
+      defaults: newLike,
+    }).then((result) => {
+      // isNewRecord?
+      if (!result[1]) {
+        Likes.destroy({ where: { id: result[0].dataValues.id } }); // 좋아요가 있을 경우 취소
+        return false;
+      } else return true;
+    });
+
     res.status(201).json({ liked });
   } catch (err) {
-    await Likes.destory({
-      where: { userId, targetId },
-    });
+    if (liked) {
+      await Likes.destroy({ where: newLike });
+    } else {
+      await Likes.create(newLike);
+    }
     await res.status(500).send(err);
   }
   res.end();
