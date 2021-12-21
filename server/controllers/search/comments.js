a755aeb6bdb789ac44226a1b83eb6a8e03e8cec2
Wi <widalida26@gmail.com>
2021-12-21 20:44:41 +0900

    Update : Get comments reafactoring

--- server/controllers/search/comments.js
+++ server/controllers/search/comments.js
@@ -9,45 +9,54 @@ const { Comments } = require('../../models');
 module.exports = async (req, res) => {
   try {
     let episodeId = req.query['episode-id'];
-    // 댓글 정보 검색
-    const searchedComments = await Comments.findAll({
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              `(SELECT COUNT(*)
+  } catch (err) {
+    res.status(400).send('Please provide all necessary information');
+  }
+
+  // 댓글 정보 검색
+  const searchedComments = await Comments.findAll({
+    attributes: {
+      include: [
+        [
+          sequelize.literal(
+            `(SELECT COUNT(*)
                     FROM Likes
                     WHERE
                     Comments.id = Likes.targetId && 
                     Comments.userId=Likes.userId)`
-            ),
-            'likeNum',
-          ],
+          ),
+          'likeNum',
         ],
-      },
-      where: { episodeId, parentCommentId: null },
-      order: [['createdAt', 'DESC']],
-    });
-
-    // 답글 개수 조회
-    const replyNums = await Comments.findAll({
-      attributes: [
-        'parentCommentId',
-        [sequelize.fn('COUNT', 'parentCommentId'), 'replyNum'],
       ],
-      where: { parentCommentId: { [Op.ne]: null } },
-      group: ['parentCommentId'],
-    }).then((result) => {
+    },
+    where: { episodeId, parentCommentId: null },
+    order: [['createdAt', 'DESC']],
+  })
+    .then((result) => result)
+    .catch((err) => res.status(500).send(err));
+
+  // 답글 개수 조회
+  const replyNums = await Comments.findAll({
+    attributes: [
+      'parentCommentId',
+      [sequelize.fn('COUNT', 'parentCommentId'), 'replyNum'],
+    ],
+    where: { parentCommentId: { [Op.ne]: null } },
+    group: ['parentCommentId'],
+  })
+    .then((result) => {
       let cnt = {};
       result.forEach((data) => {
         const { parentCommentId, replyNum } = data.dataValues;
         cnt[parentCommentId] = replyNum;
       });
       return cnt;
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 
-    //
-
+  try {
     // 응답 객체 세팅 => 댓글 정보
     let commentArr = searchedComments.map((comment) => {
       let commentResponse = ({
@@ -66,10 +75,8 @@ module.exports = async (req, res) => {
     });
     //
     // likes : [commentId, commentId, ...]
-
     res.status(200).json({ comments: commentArr });
   } catch (err) {
-    console.log(err);
     res.status(500).send(err);
   }
 };
