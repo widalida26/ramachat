86f679aa294c53fc4ce52f51cf8f0d96a117318c
Wi <widalida26@gmail.com>
2021-12-23 02:09:42 +0900

    Update : Password validation

--- client/src/pages/MyPagePersonal.js
+++ client/src/pages/MyPagePersonal.js
@@ -34,6 +34,17 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
   const email = myPageInfo ? myPageInfo.email : '';
   const token = tokenState ? tokenState : sessionStorage.getItem('token');
 
+  // *passwordRegex
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+  // *비밀번호 메시지
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
+  // *비밀번호 유효성 검사 메시지
+  const [isPassword, setIsPassword] = useState(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+  // * 모달 메시지
+  const [modalMsg, setModalMsg] = useState('');
+
   // 비밀번호 변경
   // ! 유효성 검사 추가?
   const [passwordInfo, setPasswordInfo] = useState({
@@ -46,8 +57,31 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
     setIsChange(!isChange);
   };
 
+  // const handleInputValue = (target) => (e) => {
+  //   setPasswordInfo({ ...passwordInfo, [target]: e.target.value });
+  // };
+
+  //*비밀번호 검사
   const handleInputValue = (target) => (e) => {
     setPasswordInfo({ ...passwordInfo, [target]: e.target.value });
+    if (target === 'newPassword') {
+      let passwordCurrent = e.target.value;
+      if (!passwordRegex.test(passwordCurrent)) {
+        setPasswordMessage('숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!');
+        setIsPassword(false);
+      } else {
+        setPasswordMessage('안전한 비밀번호에요 :)');
+        setIsPassword(true);
+      }
+    } else if (target === 'newPasswordConfirm') {
+      if (e.target.value === passwordInfo.newPassword) {
+        setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 :)');
+        setIsPasswordConfirm(true);
+      } else {
+        setPasswordConfirmMessage('비밀번호가 틀려요. 다시 확인해주세요!');
+        setIsPasswordConfirm(false);
+      }
+    }
   };
 
   const getMyPage = () => {
@@ -68,10 +102,10 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
   const changePassword = () => {
     // 비밀번호 변경 성공 모달띄우기...
     axios
-      .put(
+      .patch(
         `${process.env.REACT_APP_SERVER_URL}/passwordModify`,
         {
-          withCredentials: true,
+          //withCredentials: true,
           password: passwordInfo.nowPassword,
           newPassword: passwordInfo.newPassword,
         },
@@ -82,8 +116,27 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
           },
         }
       )
-      .then(() => setIsOpen(!isOpen)) // 비밀번호 변경 버튼 클릭시 모달 열기
-      .catch(() => console.log('changePassword 에러'));
+      .then(() => {
+        //* 성공 메시지
+        setModalMsg('비밀번호 변경이 완료되었습니다!');
+        setIsOpen(!isOpen);
+      }) // 비밀번호 변경 버튼 클릭시 모달 열기
+      .catch((err) => {
+        // * 에러 메시지
+        console.log(err.response.status);
+        const errCode = err.response.status;
+        console.log('is400', errCode === err.response.status);
+        if (errCode === 400) {
+          console.log('400!!');
+          setModalMsg('현재 입력하신 비밀번호가 일치하지 않습니다!');
+        } else if (errCode === 422) {
+          console.log('422');
+          setModalMsg('현재 입력하신 비밀번호가 이전 비밀번호와 같습니다!');
+        } else {
+          setModalMsg('비밀번호 변경에 실패하였습니다!');
+        }
+        console.log('changePassword 에러');
+      });
   };
 
   const signOut = () => {
@@ -136,12 +189,18 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
                 type="password"
                 handleInputValue={handleInputValue}
               ></InputForm>
+              {/* 유효성 검사 결과 메시지 */}
+              {passwordInfo.newPassword.length > 0 && <span>{passwordMessage}</span>}
               <p>New Password Confirmation</p>
               <InputForm
                 target="newPasswordConfirm"
                 type="password"
                 handleInputValue={handleInputValue}
               ></InputForm>
+              {/* 비밀번호 확인 메시지 */}
+              {passwordInfo.newPasswordConfirm.length > 0 && (
+                <span>{passwordConfirmMessage}</span>
+              )}
               <br />
               <TextButton
                 color="secondary"
@@ -154,7 +213,7 @@ export default function MyPagePersonal({ tokenState, handleLogout }) {
                 isOpen={isOpen}
                 openModalHandler={openModalHandler}
                 modalActionlHandler={openModalHandler}
-                noticeMessage={'비밀번호 변경이 완료되었습니다!'}
+                noticeMessage={modalMsg}
                 buttonMessage={'확인'}
                 endPoint={'/mypage/personal-information'}
               />
